b8a1ad9b52341a6721014ccf361c2252b0e94606
dvsii1003 <dvsmusic@gmail.com>
2024-08-07 15:24:04 -0700

    break time

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,3 +1,17 @@
-const motivationalActions = ['be ambitions', 'find inspiration around you', 'breakthrough your doubt', 'cast away your doubt', 'compliment a stranger'];
+const randomWords = {
+    noun: ['ball', 'dog', 'fan', 'guitar', 'computer', 'book'],
+    verb: ['speaks', 'writes', 'types', 'communicate', 'eats', 'sleeps'],
+    adjective: ['happy', 'bright', 'dark', 'sleepy', 'thin', 'heavy']
+};
+function randomNoun() {
+    return randomWords.noun[(Math.floor(Math.random() * randomWords.noun.length))];
+}
 const user = 'Don';
-console.log(`${user}, today you will ${motivationalActions[Math.floor(Math.random() * (motivationalActions.length - 1))]}.`);
+const noun1 = randomNoun();
+const noun2 = randomNoun();
+console.log(`${noun1} and ${noun2}`);
+
+// Present joke question
+
+
+// Present joke answer
