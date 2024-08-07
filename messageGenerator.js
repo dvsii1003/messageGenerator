75719c2727b0fd91e0ae487cdc3755ac4598d5aa
dvsii1003 <dvsmusic@gmail.com>
2024-08-07 14:19:53 -0700

    Changed motivation phrase

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,3 +1,3 @@
-const motivationalActions = ['be ambitions', 'find inspiration around me', 'breakthrough my doubt', 'cast away my doubt', 'compliment a stranger'];
+const motivationalActions = ['be ambitions', 'find inspiration around you', 'breakthrough my doubt', 'cast away my doubt', 'compliment a stranger'];
 const user = 'Don';
 console.log(`${user}, today you will ${motivationalActions[Math.floor(Math.random() * (motivationalActions.length - 1))]}.`);
