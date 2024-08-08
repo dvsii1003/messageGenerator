b291f81176021f76b335ca54c055978867a04ba4
dvsii1003 <dvsmusic@gmail.com>
2024-08-07 21:58:15 -0700

    Condensed code. Ready for review.

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,21 +1,16 @@
-const randomWords = {
+const randomVocab = {
     noun: ['ball', 'dog', 'fan', 'guitar', 'computer', 'book'],
     verb: ['smile', 'write', 'cook', 'kneel', 'eat', 'sleep'],
     adj: ['happy', 'bright', 'dark', 'sleepy', 'thin', 'heavy', 'blue']
 };
-function randomNoun() {
-    return randomWords.noun[(Math.floor(Math.random() * randomWords.noun.length))];
+function randomWord(part) {
+    return randomVocab[part] [(Math.floor(Math.random() * randomVocab[part].length))];
 };
-function randomVerb() {
-    return randomWords.verb[(Math.floor(Math.random() *randomWords.verb.length))];
-};
-function randomAdj() {
-    return randomWords.adj[(Math.floor(Math.random() * randomWords.adj.length))];
-};
+
 
 
 // Present joke question
-console.log(`Why did the ${randomNoun()} ${randomVerb()}?`);
+console.log(`Why did the ${randomWord('noun')} ${randomWord('verb')}?`);
 
 // Present joke answer
-console.log(`Because it was ${randomAdj()}.`);
+console.log(`Because it was ${randomWord('adj')}.`);
